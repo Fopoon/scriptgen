71bc92ae6464d9e76228aa411fa807da3d191894
elmernocon <elmernocon@gmail.com>
2020-03-15 22:44:54 +0800

    Updated gen_docs.py script.

--- tools/gen_docs.py
+++ tools/gen_docs.py
@@ -9,9 +9,6 @@ from scriptgen import \
     interpolate_text, \
     write_text_file
 
-from scriptgen.templates.markdown import \
-    markdown_autogen
-
 
 def get_text(
     template: str,
@@ -19,10 +16,21 @@ def get_text(
     template_name: str = None
 ) -> str:
     sb = StringBuilder()
+
+    # Add a timestamp when the template is a markdown file.
     if template_name.casefold().endswith(".md"):
+        from scriptgen.templates.markdown import markdown_autogen
         sb.wb(markdown_autogen())
         sb.nl()
-    sb.wl(interpolate_text(template, expressions))
+
+    # Replace placeholders.
+    # i.e. replace placeholders found in the text with values found in the expressions dictionary.
+    # ex: ${SOME_KEY} → ACTUAL_VALUE
+    interpolated_text = interpolate_text(template, expressions)
+
+    # Write the interpolated text into the builder.
+    sb.wl(interpolated_text)
+
     return str(sb)
 
 
@@ -31,13 +39,17 @@ if __name__ == "__main__":
 
     templates_dir_name = "templates"
 
+    # scriptgen/tools/templates/VALUES.t.json
     json_path = cwp / templates_dir_name / "VALUES.t.json"
     json_text = json_path.read_text()
     json = loads(json_text)
 
     templates = {
+        # scriptgen/tools/templates/README.t.md → scriptgen/README.md
         (cwp / templates_dir_name / "README.t.md"): (cwp.parent / "README.md"),
+        # scriptgen/tools/templates/CONTRIBUTING.t.md → scriptgen/CONTRIBUTING.md
         (cwp / templates_dir_name / "CONTRIBUTING.t.md"): (cwp.parent / "CONTRIBUTING.md"),
+        # scriptgen/tools/templates/setup.t.py → scriptgen/setup.py
         (cwp / templates_dir_name / "setup.t.py"): (cwp.parent / "setup.py")
     }
 
@@ -50,7 +62,9 @@ if __name__ == "__main__":
         write_text_file(
             text,
             target_path,
+            # checks for relevant changes between two texts to determine whether to skip writing into a file.
             diff_func=diff_text,
+            # filters out lines when checking for differences.
             filter_func=lambda line: line.startswith("[//]: # (Auto-generated"),
             log_func=lambda message: print(message)
         )
